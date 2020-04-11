b90ff5455a15e5ce53f16d40bda424c1d045de30
DELL <DELL@DELL-PC>
2020-04-11 09:47:07 +0530

    CHANGED BY DINESH

--- sprintbootapp/src/main/java/com/vidvaan/spring/boot/sprintbootapp/VidvaanClass.java
+++ sprintbootapp/src/main/java/com/vidvaan/spring/boot/sprintbootapp/VidvaanClass.java
@@ -1,5 +1,8 @@
 package com.vidvaan.spring.boot.sprintbootapp;
 
-public class VidvaanClass {
+public class VidvaanClass{
+	public static void main(String[] args) {
+		System.out.println("hai hello");
+	}
 
 }

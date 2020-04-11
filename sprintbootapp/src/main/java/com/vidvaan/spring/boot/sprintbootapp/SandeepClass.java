2c83b8347de59258f3a7b5688d55e9d570054647
konth <konth@LAPTOP-OMJBUG06>
2020-04-11 14:39:04 +0530

    add one method

--- sprintbootapp/src/main/java/com/vidvaan/spring/boot/sprintbootapp/SandeepClass.java
+++ sprintbootapp/src/main/java/com/vidvaan/spring/boot/sprintbootapp/SandeepClass.java
@@ -2,7 +2,7 @@ package com.vidvaan.spring.boot.sprintbootapp;
 
 public class SandeepClass {
 	public void name() {
-		System.out.println("welcome sandeep how do you do");
+		System.out.println("welcome sandeep how do you doooo");
 
 	}
 
